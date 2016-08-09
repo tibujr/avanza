dabfcd12b96dbeb829abef6f7cca8770fb4e8f57
tibujr <maycol_to@hotmail.com>
2016-08-09 15:51:31 -0500

    haber

--- platforms/browser/www/js/accion.js
+++ platforms/browser/www/js/accion.js
@@ -12,28 +12,22 @@ $(document).ready(function () {
 	function inicializa()
 	{
 		crearFormularioIndex();
-
-		console.log(an+" - "+al)
-		$("#inicio").css({
-			height: al+'px',
-			property2: an+'px'
-		});
-
-		$(".button-collapse").sideNav();
+		$("#inicio").css({ height: al+'px', property2: an+'px'});
+		//$(".button-collapse").sideNav();
 	}
 
 	function crearFormularioIndex()
 	{
 		var script = "<div class='input-field col s12'>";
-            script += "<input placeholder='Usuario' id='first_name' type='email' class='validate primary-text-color'>";
-            script += "<label for='first_name'>Usuario</label>";
+            script += "<input placeholder='Usuario' id='mail' type='email' class='validate primary-text-color'>";
+            script += "<label for='mail'>Usuario</label>";
             script += "</div>";
             script += "<div class='input-field col s12'>";
-            script += "<input id='last_name' type='password' class='validate primary-text-color'>",
-            script += "<label for='last_name'>Contraseña</label>";
+            script += "<input id='clave' type='password' class='validate primary-text-color'>",
+            script += "<label for='clave'>Contraseña</label>";
             script += "</div>";
-            script += "<div class='col s12'  style='font-size:12px;''>";
-            script += "<a href='#primero' class='text-primary-color waves-effect waves-light btn col s12 '>INICIAR SESIÓN</a>";
+            script += "<div class='col s12' style='font-size:12px;''>";
+            script += "<a href='#primero' class='waves-effect waves-light btn col s12 '>INICIAR SESIÓN</a>";
             script += "</div>";
          $("#cont-login").append(script);
 	}
